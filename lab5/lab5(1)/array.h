7c1edd2434222c059ffe2bfb3ddfc72093ba0dea
Marta964 <mq93162@gmail.com>
2023-07-01 23:15:56 +0300

    lab5

--- lab5/lab5(1)/array.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-void enterSize(int n);
-void memory(int* arr, int n);
-void printMassiv(int* arr, int n);
-void random(int* arr, int n);
--- lab5/lab5(3)/arrays.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-int enterSize();
-int** memory(int n);
-void printMassiv(int** arr, int n);
-void random(int** arr, int n);
-void fre(int** arr, int n);
-int MinOnMainDiaginal(int** arr, int n);
-int MinUnderMainDiaginal(int** arr, int n);
