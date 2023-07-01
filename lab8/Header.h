9b21b4377d8e6865178cd1c91c82f548697775a3
Marta964 <mq93162@gmail.com>
2023-07-01 23:17:28 +0300

    lab8

--- lab8/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-int stringLength(char* str);
-int longestString(char* str);
-void outputString(char** str, int argc);
-void insertionSort(char** str, int argc);
-void fre(char** str, int argc);
-void inputString(char*** str, int argc, char** argv);
